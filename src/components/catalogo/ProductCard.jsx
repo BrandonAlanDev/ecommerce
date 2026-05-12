89c4adaad5757ed4ab37b01f91dcb3395fe54bdd
BrandonAlanDev <brandoncarabajal@gmail.com>
2026-05-12 20:55:28 -0300

    placeholder

--- src/components/catalogo/ProductCard.jsx
+++ src/components/catalogo/ProductCard.jsx
@@ -65,7 +65,7 @@ const ProductCard = ({ product, addToCart }) => {
       {/* Image Container */}
       <div className="aspect-[4/5] overflow-hidden rounded-xl bg-gray-50 mb-4 relative">
         <Image
-          src={product.image?.[currentImage]}
+          src={(product.image && product.image !== "") ? product.image?.[currentImage] : "/placeholder.avif"}
           alt={product.title}
           width={300}
           height={300}
